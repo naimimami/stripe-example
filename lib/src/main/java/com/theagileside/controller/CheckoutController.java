54e97946f12fd042bdfc1667117a57213df13721
naimimami <naim.imami@gmail.com>
2021-10-08 22:57:42 -0400

    Initial commit

---
+++ lib/src/main/java/com/theagileside/controller/CheckoutController.java
@@ -0,0 +1,20 @@
+package com.theagileside.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.theagileside.request.ChargeRequest;
+import com.theagileside.request.ChargeRequest.Currency;
+@Controller
+public class CheckoutController {
+	@Value("${STRIPE_PUBLIC_KEY}")
+	private String stripePublicKey;
+	@RequestMapping("/checkout")
+	public String checkout(Model model) {
+		model.addAttribute("amount", 50 * 100); // in cents
+		model.addAttribute("stripePublicKey", stripePublicKey);
+		model.addAttribute("currency", ChargeRequest.Currency.USD);
+		return "checkout";
+	}
+}
